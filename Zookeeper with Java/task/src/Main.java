0b7215ef40b6c440f69da91488835442bc24c46c
Kornilenko Oleksandr <sanya.kornilenko14@gmail.com>
2024-02-21 17:28:02 +0200

    Stage 1

---
+++ Zookeeper with Java/task/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        printer();
+
+    }
+
+    static void printer(){
+        System.out.println("I love animals!");
+        System.out.println("Let's check on the animals...");
+        System.out.println("The deer looks fine.");
+        System.out.println("The bat looks happy.");
+        System.out.println("The lion looks healthy.");
+    }
+
+}
